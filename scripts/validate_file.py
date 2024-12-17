fe685bbd3b67c06cb4275753f15e5b69d0c3b65f
Andres Felipe Marciales Pardo <andres.marciales@COHGX35NHL2Q.bbrouter>
2024-12-17 12:50:41 -0500

    Changed Jenkinsfile

--- scripts/validate_file.py
+++ scripts/validate_file.py
@@ -1,31 +1,48 @@
-from great_expectations.data_context import DataContext
+# from great_expectations.data_context import DataContext
+import great_expectations as gx
+import pandas as pd
 
 
 def validate_file(file_path):
-    context = DataContext("great_expectations/")
-    checkpoint_name = "file_validation_checkpoint"
-
-    # Configurar un punto de validación
-    checkpoint = context.test_yaml_config(
-        f"""
-        name: {checkpoint_name}
-        config_version: 1.0
-        validations:
-          - batch_request:
-              datasource_name: data_dir
-              data_connector_name: default_inferred_data_connector_name
-              data_asset_name: {file_path}
-            expectation_suite_name: file_suite
-        """
+    df = pd.read_csv(
+        "{file_path}"
     )
+    context = gx.get_context()
+    data_source = context.data_sources.add_pandas("pandas")
+    data_asset = data_source.add_dataframe_asset(name="pd dataframe asset")
 
-    results = checkpoint.run()
+    batch_definition = data_asset.add_batch_definition_whole_dataframe("batch def")
+    batch = batch_definition.get_batch(batch_parameters={"dataframe": df})
+    expectation = gx.expectations.ExpectColumnValuesToBeBetween(
+        column="passenger_count", min_value=1, max_value=6
+    )
+    validation_result = batch.validate(expectation)
+    print(validation_result)
 
-    if results["success"]:
-        print(f"Validation succeeded for {file_path}")
-    else:
-        print(f"Validation failed for {file_path}")
-        raise ValueError("Validation failed!")
+    # context = DataContext("great_expectations/")
+    # checkpoint_name = "file_validation_checkpoint"
+
+    # # Configurar un punto de validación
+    # checkpoint = context.test_yaml_config(
+    #     f"""
+    #     name: {checkpoint_name}
+    #     config_version: 1.0
+    #     validations:
+    #       - batch_request:
+    #           datasource_name: data_dir
+    #           data_connector_name: default_inferred_data_connector_name
+    #           data_asset_name: {file_path}
+    #         expectation_suite_name: file_suite
+    #     """
+    # )
+
+    # results = checkpoint.run()
+
+    # if results["success"]:
+    #     print(f"Validation succeeded for {file_path}")
+    # else:
+    #     print(f"Validation failed for {file_path}")
+    #     raise ValueError("Validation failed!")
 
 
 if __name__ == "__main__":
